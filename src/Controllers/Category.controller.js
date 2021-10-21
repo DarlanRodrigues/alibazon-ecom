6b1e69c0cbb260bee42de1e30f8b37116649a9ff
root <root@DESKTOP-4KSG4D0.localdomain>
2021-10-20 22:17:34 -0300

    refactor: refact route to category

--- src/Controllers/Category.controller.js
+++ src/Controllers/Category.controller.js
@@ -4,12 +4,17 @@ import config from "../../config.js";
 axios.defaults.baseURL = config.osf_api.base_url;
 
 export const getAllCategories = async () => {
-    const categories = await axios.get(`/categories`, {
-        params: {
-            secretKey: config.osf_api.secret_key
-        }
-    });
-    return categories.data;
+    try {
+        const categories = await axios.get(`/categories`, {
+            params: {
+                secretKey: config.osf_api.secret_key
+            }
+        });
+        return categories.data;
+    } catch(error){
+        // TODO: Sentry error logging
+        return {};
+    }
 }
 
 export const getCategoriesByParentId = async (parentId = 'root') => {
--- src/Routes/Category.route.js
+++ src/Routes/Category.route.js
@@ -1,13 +1,25 @@
 import { Router } from 'express';
 import { existsSync } from 'fs';
 import { getCategoryById, getCategoriesByParentId } from '../Controllers/Category.controller.js';
+import { getProductsByCategoryId } from '../Controllers/Product.controller.js';
 
 var router = Router();
 
-router.get(['/:parentCategory'], async (req, res, next) => {
-  res.render('subcategories', { 
-    parentCategory: await getCategoryById(req.params.parentCategory),
-    categories: await getCategoriesByParentId(req.params.parentCategory),
+router.get('/*', async (req, res, next) => {
+  const reqParams = req.params[0].split('/');
+
+  // TODO: Check if parentCategory exist and subcategory belongs to it. 
+  const parentCategory  = reqParams.at(-2) || reqParams.at(-1);
+  const subCategory     = reqParams.at(-1);
+  const lastSubCategory = getProductsByCategoryId(subCategory);
+  
+  if(!lastSubCategory.hasOwnProperty('error') && subCategory.split('-').length === 3){
+    res.redirect(`/product/list/${subCategory}`)  
+  }
+  res.render('categories', { 
+    parentCategory: await getCategoryById(parentCategory),
+    categories: await getCategoriesByParentId(subCategory),
+    categorypath: req.path,
     existsSync: existsSync
   });
 });
