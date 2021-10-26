00d60d235a938b8b0b56bb336da4397cb12ca222
Darlan Rodrigues <darlanrodrigues04@gmail.com>
2021-10-25 23:50:28 -0300

    feat: add individual product page

--- src/Routes/Product.route.js
+++ src/Routes/Product.route.js
@@ -1,11 +1,12 @@
 import { Router } from 'express';
 import { getCategoryById } from '../Controllers/Category.controller.js';
-import { getAllProducts, getProductsByCategoryId } from '../Controllers/Product.controller.js';
+import { getProductsById, getProductsByCategoryId } from '../Controllers/Product.controller.js';
 
 var router = Router();
 
 router.get('/list/:parentCategory', async (req, res, next) => {
-    const category = await getCategoryById(req.params.parentCategory);
+    // For header category
+    // const category = await getCategoryById(req.params.parentCategory);
     const products = await getProductsByCategoryId(req.params.parentCategory);
 
     res.render('productsList', {
@@ -14,9 +15,17 @@ router.get('/list/:parentCategory', async (req, res, next) => {
     });
 });
 
-router.get('/:productId(\d+)', async (req, res, next) => {
+router.get('/:productId', async (req, res, next) => {
+    const products = await getProductsById(req.params.productId);
+
     res.render('product', {
-        products: await getAllProducts()
+        pageTitle: products[0].page_title,
+        productName: products[0].name,
+        productDescription: products[0].page_description,
+        productCurrency: products[0].currency,
+        productPrice: products[0].price,
+        variantionSize: products[0].variation_attributes.find( el => el.id === 'size').values,
+        variantionColor: products[0].variation_attributes.find( el => el.id === 'color').values
     });
 });
 export default router;
