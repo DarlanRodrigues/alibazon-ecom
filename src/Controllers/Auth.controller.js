a137da0400fc5ebae31812af6ac8db6c23c45bfb
Darlan Rodrigues <darlanrodrigues04@gmail.com>
2021-10-27 01:07:52 -0300

    refactor: refact signup logic to get all errors from submited form and send the to the osf api

--- src/Controllers/Auth.controller.js
+++ src/Controllers/Auth.controller.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { User } from "../Models/User.js";
+import { captureException } from '@sentry/node';
 import config from "../../config.js";
 
 axios.defaults.baseURL = config.osf_api.base_url;
@@ -7,16 +7,14 @@ axios.defaults.baseURL = config.osf_api.base_url;
 export const signUp = async (newUser) => {
     try {
         const signup = await axios.post(`/auth/signup`, {
-            params: {
-                name: newUser.name,
-                email: newUser.email,
-                password: newUser.password,
-                secretKey: config.osf_api.secret_key
-            }
+            name: newUser.name,
+            email: newUser.email,
+            password: newUser.password,
+            secretKey: config.osf_api.secret_key
         });
         return signup.status;
     } catch(error){
-        // TODO: Sentry error logging
+        captureException(error);
         return {};
     }
 }
--- src/Routes/Auth.route.js
+++ src/Routes/Auth.route.js
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 import { signUp } from '../Controllers/Auth.controller.js';
-import { check, validationResult } from 'express-validator';
+import { body, check, validationResult } from 'express-validator';
 import { User } from '../Models/User.js';
 const router = Router();
 
@@ -9,34 +9,35 @@ router.get('/signup', async (req, res, next) => {
 });
 
 router.post('/signup', [
-  check('completeName', 'The complete name must be between 3 to 45 characters long')
-    .exists()
-    .isLength({ min: 3, max: 45 }),
-  check('email', 'Email is not valid')
-    .isEmail()
-    .normalizeEmail(),
-  check('password', 'Password must be minimum 5 characters')
-    .isLength({ min: 5 }),
-  check('passwordConfirm', 'Password confirmation does not match password')
-    .isLength({ min: 5 })
-    .custom((value, { req }) => {
-      if (value !== req.body.password) {
-        throw new Error();
-      }
-      return true;
-    })
-], (req, res, next)=> {
-
-  const errors = validationResult(req)
-  if(!errors.isEmpty()) {
-      let errorsMessages = errors.array();
-      //Remove duplicates messages
-      errorsMessages = [...new Set(errorsMessages)];
-      res.render('signup', {
-        errorsMessages: errorsMessages
+    body('completeName', 'The complete name must be between 3 to 45 characters long')
+      .exists()
+      .isLength({ min: 3, max: 45 }),
+    body('email', 'Email is not valid')
+      .isEmail()
+      .normalizeEmail(),
+    body('password', 'Password must be minimum 5 characters')
+      .isLength({ min: 5 }),
+    body('passwordConfirm')
+      .custom((value, { req }) => {
+        if (value !== req.body.password) {
+          throw new Error('Password confirmation does not match password');
+        }
+        return true;
       })
-  }else{
-    signUp(new User(req.body.completeName, req.body.email, req.body.password));
-  }
+  ]
+, (req, res, next)=> {
+    let { errors } = validationResult(req)
+  
+    if(errors.length !== 0) {
+        let errorsMessages = errors.map(a => a.msg);
+        //Remove duplicates messages
+        errorsMessages = [...new Set(errorsMessages)];
+        res.render('signup', {
+          errorsMessages: errorsMessages
+        })
+    }else{
+      signUp(new User(req.body.completeName, req.body.email, req.body.password));
+      res.redirect('/');
+    }
 })
 export default router;
